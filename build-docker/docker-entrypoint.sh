70c2e8c5404ab2315c5ae2cd3691782ef352c96b
Paulo Gomes <pauloamgomes@gmail.com>
2017-01-25 23:36:45 +0000

    added drop of database during installation if present

--- build-docker/docker-entrypoint.sh
+++ build-docker/docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-MOODLE_FOLDER="/var/www/moodle/html"
-DATA_FOLDER="/var/www/moodle/data"
+ROOT_FOLDER="/var/www/moodle"
+MOODLE_FOLDER="$ROOT_FOLDER/html"
+DATA_FOLDER="$ROOT_FOLDER/data"
 
 set -e
 
@@ -10,19 +11,17 @@ if [ ! -f "$MOODLE_FOLDER/config.php" ]; then
   echo "Please wait. Preparing moodle installation."
   echo "=================================================="
 
-  if [ ! -f "/tmp/v$MOODLE_VERSION.tar.gz" ]; then
+  if [ ! -f "$ROOT_FOLDER/v$MOODLE_VERSION.tar.gz" ]; then
     echo "Moodle not found! Downloading from https://github.com/moodle/moodle/archive/v$MOODLE_VERSION.tar.gz"
-    wget https://github.com/moodle/moodle/archive/v$MOODLE_VERSION.tar.gz -P /tmp
+    wget https://github.com/moodle/moodle/archive/v$MOODLE_VERSION.tar.gz -P $ROOT_FOLDER
   fi
 
   [ -d $MOODLE_FOLDER ] && rm -rf $MOODLE_FOLDER
+  [ -d DATA_FOLDER ] && rm -rf DATA_FOLDER/*
 
-  tar xfz /tmp/v$MOODLE_VERSION.tar.gz -C /tmp
-  mv /tmp/moodle-$MOODLE_VERSION $MOODLE_FOLDER
+  tar xfz $ROOT_FOLDER/v$MOODLE_VERSION.tar.gz -C /var/www/moodle && mv $ROOT_FOLDER/moodle-$MOODLE_VERSION $MOODLE_FOLDER
 
-  [ -d "$DATA_FOLDER" ] || mkdir $DATA_FOLDER
-
-  chown -R www-data:www-data $MOODLE_FOLDER $DATA_FOLDER
+  mysql -h moodle_db -u root $MOODLE_DB_NAME && mysql -h moodle_db -u root -e "DROP DATABASE $MOODLE_DB_NAME" || echo "ok"
 
   echo "=================================================="
   echo "Installing moodle... this can take a while."
@@ -42,13 +41,13 @@ if [ ! -f "$MOODLE_FOLDER/config.php" ]; then
     --non-interactive
   if [ $? -eq 0 ]; then
     echo "=================================================="
-    chown -R www-data:www-data /var/www/moodle
+    chown -R www-data:www-data $MOODLE_FOLDER $DATA_FOLDER
     echo "INSTALLATION COMPLETE!"
     echo
-    echo "url: http://localhost"
+    echo "url: http://$MOODLE_HOSTNAME"
     echo
-    echo "admin user: admin"
-    echo "admin pass: admin123"
+    echo "admin user: $MOODLE_USER"
+    echo "admin pass: $MOODLE_PASS"
     echo "=================================================="
   else
     echo "=================================================="
